f9ff40b9756ac455d4ba92f9a5dbe7cd8d12c7da
Sameer Jagtap <jagtap.sameer00@gmail.com>
2023-12-14 18:22:15 +0530

    Added MVP Example

---
+++ mvp/mvp/Views/TrafficLightViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class TrafficLightViewController: UIViewController, TrafficLightViewDelegate {
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    private let trafficLightPresenter = TrafficLightPresenter(trafficLightService: TrafficLightService())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        trafficLightPresenter.setViewDelegate(trafficLightViewDelegate: self)
+    }
+    
+    @IBAction func redLightAction(_ sender: Any) {
+        trafficLightPresenter.trafficLightColorSelected(colorName:"Red")
+    }
+    
+    @IBAction func yellowLightAction(_ sender: Any) {
+        trafficLightPresenter.trafficLightColorSelected(colorName:"Yellow")
+    }
+    
+    @IBAction func greenLightAction(_ sender: Any) {
+        trafficLightPresenter.trafficLightColorSelected(colorName:"Green")
+    }
+    
+    func displayTrafficLight(description:(String)) {
+        descriptionLabel.text = description
+    }
+}
